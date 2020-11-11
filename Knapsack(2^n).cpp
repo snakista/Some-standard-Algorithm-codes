d336a8bccb72c82e804e738c554df80d296a134d
Sitanshu Yadav <sitanshyadav@gmail.com>
2020-11-11 15:42:14 +0530

    Update Knapsack(2^n).cpp

--- Knapsack(2^n).cpp
+++ Knapsack(2^n).cpp
@@ -1,1 +1,24 @@
 
+#include <iostream>
+
+using namespace std;
+int knap(int cap,int val[],int wt[],int n)
+{
+    if(cap<=0||n<=0)
+    return 0;
+    if(wt[n-1]>cap)
+    return knap(cap,val,wt,n-1);
+    else
+    {
+        return max(val[n-1]+knap(cap-wt[n-1],val,wt,n-1),knap(cap,val,wt,n-1));
+    }
+}
+int main()
+{
+    int val[]={60,100,120};
+    int wt[]={10,20,30};
+    int cap=50;
+    int n=3;
+    cout<<knap(cap,val,wt,n);
+    return 0;
+}
